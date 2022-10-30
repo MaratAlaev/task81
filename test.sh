ecc0c4e64480219ec3a04861644758faf805a353
MaratAlaev <lots_hell@mail.ru>
2022-10-30 18:36:36 +0300

    adding functionality to the test.sh script

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+workdir=$1
+
+for var in $(ls $workdir)
+do
+if  [ -f "$var" ] && [ ! -L "$var" ]; then
+let sumsizefile+=$(stat -c "%s" $var)
+let count+=1
+fi
+done
+
+echo Средний размер файла в дериктории:
+expr $sumsizefile / $count
